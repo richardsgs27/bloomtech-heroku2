2bc7a9e2f5b8905b0bebcc7e4114198b7f448941
Gigi <gigi@iMacs-iMac.local>
2022-04-15 01:26:41 -0500

    Commiting

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+
+server.get('/', (req, res) => {
+    //res.json('Hello world')
+    res.json({MESSAGE: process.env.MESSAGE})
+})
+
+module.exports = server
